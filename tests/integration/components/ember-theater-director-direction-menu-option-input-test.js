f1f6664eabe935ff2f4ba9c19a68206abbc332f1
null null null <null@ember.theater>
2016-06-08 08:51:45 -0700

    add basic integration tests for input

--- tests/integration/components/ember-theater-director-direction-menu-option-input-test.js
+++ tests/integration/components/ember-theater-director-direction-menu-option-input-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { initialize as initializeHook } from 'ember-hook';
+import { hook, initialize as initializeHook } from 'ember-hook';
 import { initialize as initializeMultitons } from 'ember-multiton-service';
 
 const {
-  getOwner
+  getOwner,
+  setProperties
 } = Ember;
 
 moduleForComponent('ember-theater-director-direction-menu-option-input', 'Integration | Component | ember theater director direction menu option input', {
@@ -19,11 +20,27 @@ moduleForComponent('ember-theater-director-direction-menu-option-input', 'Integr
   }
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it auto focuses itself', function(assert) {
+  assert.expect(1);
 
   this.render(hbs`{{ember-theater-director-direction-menu-option-input}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.ok(this.$(hook('menu_direction_option_input')).is(':focus'), 'it is focused');
+});
+
+test('it triggers `toggleInput` on `focusOut`', function(assert) {
+  assert.expect(1);
+
+  setProperties(this, {
+    toggleInput() {
+      assert.ok(true, 'toggleInput was called')
+    },
+    choose() {
+      assert.ok(false, 'choose was called');
+    }
+  });
+
+  this.render(hbs`{{ember-theater-director-direction-menu-option-input toggleInput=(action toggleInput) choose=(action choose)}}`);
+
+  this.$(hook('menu_direction_option_input')).blur();
 });
