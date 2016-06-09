02ca62a3cbffe4e7e7355ab975706eb15b3366c9
null null null <null@ember.theater>
2016-06-09 08:21:34 -0700

    add option tests

--- tests/integration/components/ember-theater-director-direction-menu-option-test.js
+++ tests/integration/components/ember-theater-director-direction-menu-option-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { initialize as initializeHook } from 'ember-hook';
+import { hook, initialize as initializeHook } from 'ember-hook';
 import { initialize as initializeMultitons } from 'ember-multiton-service';
 
 const {
-  getOwner
+  getOwner,
+  setProperties
 } = Ember;
 
 moduleForComponent('ember-theater-director-direction-menu-option', 'Integration | Component | ember theater director direction menu option', {
@@ -19,11 +20,74 @@ moduleForComponent('ember-theater-director-direction-menu-option', 'Integration 
   }
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders a button when both `choice.inputable` and `inputOpen` are false', function(assert) {
+  assert.expect(2);
 
-  this.render(hbs`{{ember-theater-director-direction-menu-option}}`);
+  const choice = Ember.Object.create({
+    inputable: false
+  });
 
-  assert.equal(this.$().text().trim(), '');
+  setProperties(this, {
+    choice,
+    inputOpen: false
+  });
+
+  this.render(hbs`{{ember-theater-director-direction-menu-option choice=choice inputOpen=inputOpen}}`);
+
+  assert.ok(this.$(hook('menu_direction_option_button')).length === 1, 'it renders a button');
+  assert.ok(this.$(hook('menu_direction_option_input')).length === 0, 'it does not render an input');
+});
+
+test('it renders a button when is `choice.inputable` true but `inputOpen` are false', function(assert) {
+  assert.expect(2);
+
+  const choice = Ember.Object.create({
+    inputable: true
+  });
+
+  setProperties(this, {
+    choice,
+    inputOpen: false
+  });
+
+  this.render(hbs`{{ember-theater-director-direction-menu-option choice=choice inputOpen=inputOpen}}`);
+
+  assert.ok(this.$(hook('menu_direction_option_button')).length === 1, 'it renders a button');
+  assert.ok(this.$(hook('menu_direction_option_input')).length === 0, 'it does not render an input');
+});
+
+test('it renders a button when both `choice.inputable` is false but `inputOpen` is true', function(assert) {
+  assert.expect(2);
+
+  const choice = Ember.Object.create({
+    inputable: false
+  });
+
+  setProperties(this, {
+    choice,
+    inputOpen: true
+  });
+
+  this.render(hbs`{{ember-theater-director-direction-menu-option choice=choice inputOpen=inputOpen}}`);
+
+  assert.ok(this.$(hook('menu_direction_option_button')).length === 1, 'it renders a button');
+  assert.ok(this.$(hook('menu_direction_option_input')).length === 0, 'it does not render an input');
+});
+
+test('it renders an input when both `choice.inputable` and `inputOpen` are true', function(assert) {
+  assert.expect(2);
+
+  const choice = Ember.Object.create({
+    inputable: true
+  });
+
+  setProperties(this, {
+    choice,
+    inputOpen: true
+  });
+
+  this.render(hbs`{{ember-theater-director-direction-menu-option choice=choice inputOpen=inputOpen}}`);
+
+  assert.ok(this.$(hook('menu_direction_option_button')).length === 0, 'it does not render a button');
+  assert.ok(this.$(hook('menu_direction_option_input')).length === 1, 'it renders an input');
 });
