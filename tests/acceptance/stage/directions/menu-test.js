3ebf2b132e9c325d3a40ea54a34b0c5d30ca1763
null null null <null@affinitygamescollective.org>
2016-07-18 20:17:47 -0700

    update menu

--- tests/acceptance/stage/directions/menu-test.js
+++ tests/acceptance/stage/directions/menu-test.js
@@ -18,34 +18,34 @@ test('Affinity Engine | Director | Directions | Menu', function(assert) {
 
   visit('/affinity-engine/test-scenarios/stage/directions/menu').then(() => {
     assert.equal($hook('menu_direction').length, 1, 'menu is rendered');
-    assert.equal($hook('ember_columnar_menu_option').length, 3, 'options are rendered');
-    assert.equal($hook('ember_columnar_menu_option_button').length, 3, 'options default to buttons');
-    assert.ok($hook('ember_columnar_menu').hasClass('ps-container'), 'perfect scrollbar was applied');
+    assert.equal($hook('ember_flex_menu_option').length, 3, 'options are rendered');
+    assert.equal($hook('ember_flex_menu_option_button').length, 3, 'options default to buttons');
+    assert.ok($hook('ember_flex_menu').hasClass('ps-container'), 'perfect scrollbar was applied');
 
-    return click(Ember.$(`${hook('ember_columnar_menu_option_button')}:nth(1)`));
+    return click(Ember.$(`${hook('ember_flex_menu_option_button')}:nth(1)`));
   }).then(() => {
     assert.equal($hook('data').text().trim(), 'key: 1, text: B', 'first result correct');
 
-    return click(Ember.$(`${hook('ember_columnar_menu_option_button')}:nth(2)`));
+    return click(Ember.$(`${hook('ember_flex_menu_option_button')}:nth(2)`));
   }).then(() => {
     assert.equal($hook('data').text().trim(), 'key: keyC, text: textC', 'second result correct');
 
-    return click(Ember.$(`${hook('ember_columnar_menu_option_button')}:nth(0)`));
+    return click(Ember.$(`${hook('ember_flex_menu_option_button')}:nth(0)`));
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_input').length, 1, 'input is rendered');
-    assert.equal($hook('ember_columnar_menu_option_button').length, 2, 'button is removed');
+    assert.equal($hook('ember_flex_menu_option_input').length, 1, 'input is rendered');
+    assert.equal($hook('ember_flex_menu_option_button').length, 2, 'button is removed');
 
-    return fillIn(hook('ember_columnar_menu_option_input'), 'foo');
+    return fillIn(hook('ember_flex_menu_option_input'), 'foo');
   }).then(() => {
-    return triggerEvent(hook('ember_columnar_menu_option_input'), 'blur');
+    return triggerEvent(hook('ember_flex_menu_option_input'), 'blur');
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_input').length, 0, 'input is removed');
-    assert.equal($hook('ember_columnar_menu_option_button').length, 3, 'button is restored');
+    assert.equal($hook('ember_flex_menu_option_input').length, 0, 'input is removed');
+    assert.equal($hook('ember_flex_menu_option_button').length, 3, 'button is restored');
 
-    return click(Ember.$(`${hook('ember_columnar_menu_option_button')}:nth(0)`));
+    return click(Ember.$(`${hook('ember_flex_menu_option_button')}:nth(0)`));
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_input').length, 1, 'input is restored');
-    assert.equal($hook('ember_columnar_menu_option_input').val(), 'foo', 'input value persisted');
+    assert.equal($hook('ember_flex_menu_option_input').length, 1, 'input is restored');
+    assert.equal($hook('ember_flex_menu_option_input').val(), 'foo', 'input value persisted');
 
     return keyDown('Enter');
   }).then(() => {
@@ -55,22 +55,22 @@ test('Affinity Engine | Director | Directions | Menu', function(assert) {
     assert.equal($hook('menu_direction_header').text().trim(), 'foo', 'header is set correctly');
     assert.ok($hook('menu_direction').hasClass('bar'), 'className bar added correctly');
     assert.ok($hook('menu_direction').hasClass('baz'), 'className baz added correctly');
-    assert.equal($hook('ember_columnar_menu_option_button').get(0), document.activeElement, 'the first button is auto-focused');
+    assert.equal($hook('ember_flex_menu_option_button').get(0), document.activeElement, 'the first button is auto-focused');
 
     return keyDown('ArrowDown');
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_button').get(1), document.activeElement, 'ArrowDown moves the focus down');
+    assert.equal($hook('ember_flex_menu_option_button').get(1), document.activeElement, 'ArrowDown moves the focus down');
 
     return keyDown('ArrowUp');
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_button').get(0), document.activeElement, 'ArrowUp moves the focus up');
+    assert.equal($hook('ember_flex_menu_option_button').get(0), document.activeElement, 'ArrowUp moves the focus up');
 
     return keyDown('ArrowUp');
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_button').get(2), document.activeElement, 'ArrowUp rolls over to the bottom');
+    assert.equal($hook('ember_flex_menu_option_button').get(2), document.activeElement, 'ArrowUp rolls over to the bottom');
 
     return keyDown('ArrowDown');
   }).then(() => {
-    assert.equal($hook('ember_columnar_menu_option_button').get(0), document.activeElement, 'ArrowDown rolls over to the top');
+    assert.equal($hook('ember_flex_menu_option_button').get(0), document.activeElement, 'ArrowDown rolls over to the top');
   });
 });
