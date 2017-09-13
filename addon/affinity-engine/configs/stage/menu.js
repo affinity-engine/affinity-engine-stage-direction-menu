083831be9cbc5821b433a9519b9748a3f9c1d450
null null null <null@affinitygamescollective.org>
2017-09-13 13:20:42 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/stage/menu.js
+++ addon/affinity-engine/configs/stage/menu.js
@@ -1,23 +1,25 @@
 export default {
   priority: 3,
-  component: {
-    stage: {
-      direction: {
-        menu: {
-          attrs: {
-            layer: 'stage.prompt.menu'
-          }
-        }
-      },
-      layer: {
-        stage: {
-          prompt: {
+  default: {
+    component: {
+      stage: {
+        direction: {
+          menu: {
             attrs: {
-              zIndex: 10
-            },
-            menu: {
+              layer: 'stage.prompt.menu'
+            }
+          }
+        },
+        layer: {
+          stage: {
+            prompt: {
               attrs: {
                 zIndex: 10
+              },
+              menu: {
+                attrs: {
+                  zIndex: 10
+                }
               }
             }
           }
